4e3e7d7c2b6d3dbbcca344736a8a4f76c249e0a6
Keivalya Pandya <61532731+keivalya@users.noreply.github.com>
2025-01-26 17:37:17 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ from gradio.themes import Soft
 import os
 from dotenv import load_dotenv
 from transcript import transcribe_audio  # Import the transcription function
+import numpy as np
+import requests
+from scipy.io.wavfile import write
 
 # Load environment variables from .env file
 load_dotenv()
@@ -11,15 +14,33 @@ def analyze_song(mp3_file):
     # Call the transcription function and get the transcription
     transcription = transcribe_audio(mp3_file)  # Pass the mp3 filename to transcript.py
 
+    os.makedirs("out", exist_ok=True)
+    write('test.wav', audio[0], audio[1])
+    data = {
+        'api_token': '171afdabea68ffcdbd7f102b8611ac63',
+        'return': 'apple_music,spotify',
+    }
+    files = {
+        'file': open('test.wav', 'rb'),
+    }
+    result = requests.post('https://api.audd.io/', data=data, files=files)
+    if result is not None:
+        artist = result.json()['result']['artist']
+        title = result.json()['result']['title']
+        genre = result.json()['result']['apple_music']['genreNames']
+        image_url = result.json()['result']['spotify']['album']['images'][0]['url']
+    else:
+        artist, title, genre, image_url = None, None, None, None
+
     # Placeholder for song analysis logic
-    title = "Sample Title"
-    artist = "Sample Artist"
-    genre = "Sample Genre"
+    # title = "Sample Title"
+    # artist = "Sample Artist"
+    # genre = "Sample Genre"
     instrumentation = "Sample Instrumentation"
     lyrics = transcription  # Use the transcription as lyrics
     tempo_key = "Sample Tempo/Key"
     
-    return title, artist, genre, instrumentation, lyrics, tempo_key, mp3_file  # Return the mp3 file for replay
+    return title, artist, genre, instrumentation, lyrics, tempo_key, mp3_file, image_url  # Return the mp3 file for replay
 
 # Custom CSS to set a fun musical theme image as background
 css = """
@@ -42,6 +63,7 @@ demo = gr.Interface(
         gr.Textbox(label="Lyrics"),
         gr.Textbox(label="Tempo/Key"),
         gr.Audio(label="Replay Recorded Audio")  # Add an output for replaying the recorded audio
+        gr.Image(label="Cover")  # Gives the Cover image
     ],
     theme=Soft(),  # Apply the Soft theme
     title="Concert Buddy 😄🎵🪩"  # Apply custom CSS for background image
